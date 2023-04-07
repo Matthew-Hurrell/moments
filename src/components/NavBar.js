fe5a04d0b7f7edf1378af64beb10056815d663a2
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-04-07 17:35:03 +0000

    Add basic routing with navbar and styling

---
+++ src/components/NavBar.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Navbar, Container, Nav } from "react-bootstrap";
+import moments_logo from '../assets/moments_logo.png';
+import styles from '../styles/NavBar.module.css';
+import { NavLink } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
+        <Container>
+            <NavLink to="/">
+              <Navbar.Brand>
+                  <img src={moments_logo} alt="moments logo" height="45" />
+            </Navbar.Brand>
+            </NavLink>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ml-auto text-left">
+                <NavLink 
+                  exact
+                  className={styles.NavLink} 
+                  activeClassName={styles.Active} 
+                  to="/"
+                >
+                  <i className="fa-solid fa-house"></i> Home
+                </NavLink>
+                <NavLink 
+                  className={styles.NavLink} 
+                  activeClassName={styles.Active} 
+                  to="/signin"
+                >
+                  <i className="fa-solid fa-right-to-bracket"></i> Sign in
+                </NavLink>
+                <NavLink
+                  exact
+                  className={styles.NavLink}
+                  activeClassName={styles.Active}
+                  to="/signup"
+                >
+                  <i className="fa-solid fa-user-plus"></i> Sign up
+                </NavLink>
+                </Nav>
+            </Navbar.Collapse>
+        </Container>
+    </Navbar>
+  )
+}
+
+export default NavBar
