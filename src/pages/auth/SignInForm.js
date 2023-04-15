9b815b9cb9dd5a518e7bc3038053bdf62bb45d80
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-04-15 11:38:50 +0000

    Add token timestamp to local storage to resolve console errors

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -17,6 +17,7 @@ import appStyles from "../../App.module.css";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import { useSetCurrentUser } from "../../contexts/CurrentUserContext";
 import { useRedirect } from "../../hooks/useRedirect";
+import { setTokenTimestamp } from "../../utils/utils";
 
 function SignInForm() {
   const setCurrentUser = useSetCurrentUser();
@@ -45,6 +46,7 @@ function SignInForm() {
         try {
             const {data} = await axios.post('/dj-rest-auth/login/', signInData);
             setCurrentUser(data.user);
+            setTokenTimestamp(data);
             history.goBack();
         } catch(err) {
             setErrors(err.response?.data);
